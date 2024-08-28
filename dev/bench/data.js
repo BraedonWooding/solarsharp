90f6ec0d53d6db1a996540d948c9b8123cd8038f
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 14:35:52 +0000

    add Benchmark (benchmarkdotnet) benchmark result for d5b26052d9702b7f6ef0846abb5338e9a8c9331b

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724855752031,
+  "repoUrl": "https://github.com/BraedonWooding/solarsharp",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "Braedonww@gmail.com",
+            "name": "Braedon",
+            "username": "BraedonWooding"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d5b26052d9702b7f6ef0846abb5338e9a8c9331b",
+          "message": "Update main.yml",
+          "timestamp": "2024-08-29T00:33:19+10:00",
+          "tree_id": "dd73c6e8f01376a9bc4c2aa03d2e9d7edd8577df",
+          "url": "https://github.com/BraedonWooding/solarsharp/commit/d5b26052d9702b7f6ef0846abb5338e9a8c9331b"
+        },
+        "date": 1724855751159,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmark.Benchmarks.Benchmark(Implementation: KeraImplementation, Test: binarytrees.lua-2.lua)",
+            "value": 1598800.7864118305,
+            "unit": "ns",
+            "range": "± 10185.108440357035"
+          },
+          {
+            "name": "Benchmark.Benchmarks.Benchmark(Implementation: MoonSharpImplementation, Test: binarytrees.lua-2.lua)",
+            "value": 6364582.137867647,
+            "unit": "ns",
+            "range": "± 130190.38609300366"
+          },
+          {
+            "name": "Benchmark.Benchmarks.Benchmark(Implementation: NLuaImplementation, Test: binarytrees.lua-2.lua)",
+            "value": 1608893.1978824013,
+            "unit": "ns",
+            "range": "± 13677.649606849993"
+          },
+          {
+            "name": "Benchmark.Benchmarks.Benchmark(Implementation: SolarSharpImplementation, Test: binarytrees.lua-2.lua)",
+            "value": 4387640.215401785,
+            "unit": "ns",
+            "range": "± 28332.789281425303"
+          }
+        ]
+      }
+    ]
+  }
+}
